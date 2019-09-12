02ab89e041a54c6806d1ef77edd9cb7bb58d3fb6
郑朝军 <1250393285@qq.com>
2019-09-12 10:22:27 +0800

    正常提交

--- lib/zpub_bas.dart
+++ lib/zpub_bas.dart
@@ -3,26 +3,14 @@ library zpub_bas;
 // FuctionStateFul基类
 export './com/zerogis/zpubbase/plugin/FuctionStateFulBase.dart';
 
-
-
-
 // 组件库,用于创建常用的组件：类似Android中layout文件中common_progressbar.xml相关
 export './com/zerogis/zpubbase/core/SysWidgetCreator.dart';
-
-
-
 
 // 数据响应事件
 export './com/zerogis/zpubbase/listener/HttpListener.dart';
 
-
-
-
 //activity自定义栈
 export './com/zerogis/zpubbase/manager/StateManager.dart';
-
-
-
 
 // 文本工具类
 export './com/zerogis/zpubbase/util/CxTextUtil.dart';
@@ -35,11 +23,8 @@ export './com/zerogis/zpubbase/util/AssetsUtil.dart';
 // 字符串工具类
 export './com/zerogis/zpubbase/util/StringUtil.dart';
 
-
-
 // 颜色类似Android资源color.xml文件相关
 export './com/zerogis/zpubbase/resource/ColorRes.dart';
-
 
 
 // 注册中心服务基类
